06b432a58fae0b9f2139701d67ad50a12fcaea8e
Amanda Santos Maciel <97988881+Amanda-Maciel@users.noreply.github.com>
2025-08-14 20:11:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, jsonify, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Olá, mundo! Seu Flask está funcionando."
+    return 'Hello World! Disciplina PTBDSWS'
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/user/Fabio Teixeira')
+def user():
+    return 'Hello, Fabio Teixeira!'
+
+@app.route('/contextorequisicao')
+def contexto():
+    return 'Your browser is Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
+
+@app.route('/codigostatusdiferente')
+def status_diferente():
+    return 'Bad Request'
+
+@app.route('/objetoresposta')
+def objeto_resposta():
+    return jsonify({'This document carries a cookie!'})
+
+@app.route('/redirecionamento')
+def redirecionamento():
+    return redirect(url_for('https://ptb.ifsp.edu.br/'))
+
+@app.route('/abortar')
+def rota_abortada():
+    abort(403)  
+
